b941cbdc731b170b5882c978398969069a9285cc
Slava Zavhorodnii <zavgorodniy@gmail.com>
2020-01-30 08:56:42 +0200

    Commit to Dev branch

--- src/name/zavgorodnii/Main.java
+++ src/name/zavgorodnii/Main.java
@@ -6,22 +6,22 @@ public class Main {
 
     public static void main(String[] args) {
         MyStack<Integer> stack = new MyStackImpl<>(5, Integer.class);
-        stack.push(1);
+        stack.push(2);
         System.out.println(stack.empty());
         System.out.println("Stack = " + stack.peek());
 
-        stack.push(2);
+        stack.push(1);
         System.out.println("Stack = " + stack.peek());
 
         stack.push(3);
         System.out.println("Stack = " + stack.peek());
+        System.out.println("Min element = " + stack.getMin());
         System.out.println(stack.search(2));
         System.out.println("===============");
         System.out.println(stack.pop());
         System.out.println(stack.peek());
         System.out.println(stack.pop());
         System.out.println(stack.peek());
-        stack.pop();
         stack.pop();
         System.out.println(stack.empty());
     }
